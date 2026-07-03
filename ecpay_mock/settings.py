88fccfa9553fd295f971ff7bad7df0cb347b39b4
luffy <monzid1@users.noreply.huggingface.co>
2026-07-03 12:54:57 +0000

    Update ecpay_mock/settings.py

--- ecpay_mock/settings.py
+++ ecpay_mock/settings.py
@@ -46,7 +46,7 @@ else:
     BACKEND_URL = "http://127.0.0.1:8001"
 
 ALLOWED_HOSTS = [
-	'monzid1-ecpay_mock.hf.space',
+	'monzid1-ecpay-mock.hf.space',
 	'proxy.spaces.internal.huggingface.tech'
 ]
 
